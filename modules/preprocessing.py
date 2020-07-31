a48519d6180f733c8beed66be36c92410b26d41d
Orion Dollar <oriondollar@Orions-MacBook-Pro-2.local>
2020-07-30 20:18:23 -0700

    preprocessing updates

--- modules/preprocessing.py
+++ modules/preprocessing.py
@@ -1,3 +1,10 @@
+import io
+import os
+import sys
+import re
+import json
+import regex
+import pickle
 import numpy as np
 import pandas as pd
 
@@ -11,10 +18,10 @@ class PreProcessor():
         """
         Parameters:
             texts (list, required): list of texts to be preprocessed
-            type (str, required): type of text to be processed (abstracts or
-                                  full text)
+            type (str): type of text to be processed (abstracts or full text)
         """
         self.type = type
+        self.min_len = 50
         self.ELEMENTS = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K",
                          "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
                          "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I",
@@ -38,18 +45,182 @@ class PreProcessor():
                               "mendelevium", "nobelium", "lawrencium", "rutherfordium", "dubnium", "seaborgium", "bohrium",
                               "hassium", "meitnerium", "darmstadtium", "roentgenium", "copernicium", "nihonium", "flerovium",
                               "moscovium", "livermorium", "tennessine", "oganesson", "ununennium"]
-        if self.type == 'abstract':
+
+        self.element_dict = {}
+        for element, name in zip(self.ELEMENTS, self.ELEMENT_NAMES):
+            self.element_dict[element] = name
+
+        self.FORMULA_REGX = regex.compile(r"^("+r"|".join(self.ELEMENTS) +
+                                          r"|[0-9])+$")
+        self.VALENCE_REGX = regex.compile(r"^("+r"|".join(self.ELEMENTS) +
+                                          r")(\(([IV|iv]|[Vv]?[Ii]{0,3})\))$")
+
+        if self.type == 'abstract': ### Clean abstracts
             self.clean_abstracts = []
-            for abstract in texts:
-                assert abstract is not None, "NONETYPE"
-                abstract = self.clean_abstract(abstract)
-                self.clean_abstracts.append(abstract)
+            self.dropped_idxs = []
+            warned = False
+            for i, abstract in enumerate(texts):
+                if abstract is None:
+                    sys.stdout.write("\r\033[K"+"WARNING: TEXT LIST CONTAINS EMPTY ABSTRACTS. NOT ALL SAMPLES CAN BE CLEANED.")
+                    warned = True
+                else:
+                    abstract = self.clean_abstract(abstract)
+                    if len(abstract) < self.min_len:
+                        self.dropped_idxs.append(i)
+                    else:
+                        self.clean_abstracts.append(abstract)
+            if warned:
+                print('\n')
 
+        self.entity_to_cid = {}
+        self.cid_to_synonyms = {}
 
-    def gather_chemical_entities(self, abstracts):
+    def load_search_history(self, path):
+        with open(path) as f:
+            search_history = json.load(f)
+        self.entity_to_cid = search_history['entity_to_cid']
+        self.cid_to_synonyms = search_history['cid_to_synonyms']
+
+    def normalize_chemical_entities(self, abstracts, remove_abbreviations=True, verbose=False, write_bold=False, save=False):
+        """
+        Iterates through abstracts, extracts chemical entities and normalizes
+        them
+
+        Parameters:
+            abstracts (list, required): List of abstracts to normalize
+            remove_abbreviations (bool): If true then replace abbreviated
+                                         entities with full name (only those
+                                         extracted by chemdataextractor)
+            verbose (bool): If true then prints abstract pre- and post-processing
+            write_bold (bool): If true then prints chemical entities bolded to terminal
+            save (bool): If true then saves abstracts, search history and run history
+                         to preprocessor_files folder. WARNING: Running this function
+                         twice without moving saved files will overwrite your previous
+                         saves
+        """
+
+        self.normalized_abstracts = []
+        self.entity_counts = {}
+        self.entities_per_abstract = {}
+
+        # Some entity names are ambiguous and must be hard coded
+        self.entity_to_cid['CO'] = [281, 'carbon monoxide']
+        self.entity_to_cid['Co'] = [104730, 'cobalt']
+        self.entity_to_cid['NO'] = [145068, 'nitric oxide']
+        self.entity_to_cid['No'] = [24822, 'nobelium']
+        self.entity_to_cid['sugar'] = [None, None]
+        self.cid_to_synonyms[281] = ['CO']
+        self.cid_to_synonyms[104730] = ['Co']
+        self.cid_to_synonyms[145068] = ['NO']
+        self.cid_to_synonyms[24822] = ['No']
+        self.entity_counts['carbon monoxide'] = 1
+        self.entity_counts['cobalt'] = 1
+        self.entity_counts['nitric oxide'] = 1
+        self.entity_counts['nobelium'] = 1
+        self.entity_counts['sugar'] = 1
         for i, abstract in enumerate(abstracts):
+            # if i % 10 == 0:
+            #     print('{} %'.format(round(i / len(abstracts) * 100, 3)))
+            ### Remove and normalize abbreviations
+            if remove_abbreviations:
+                abstract = self.remove_abbreviations(abstract)
+            else:
+                pass
+
             doc = Document(abstract)
-            print('---Abstract {}---'.format(i))
+            if verbose:
+                print('---Abstract {}---'.format(i+1))
+                print(abstract+'\n')
+            cems = doc.cems
+            entity_list = []
+            for cem in cems:
+                ### Check if abbreviated valence state
+                elem_valence = self.VALENCE_REGX.match(cem.text)
+                if elem_valence:
+                    elem = elem_valence.group(1)
+                    valence = elem_valence.group(2)
+                    elem = self.element_dict[elem]
+                    cem.text = elem+valence
+
+                ### Check if ambiguous formula
+                if self.FORMULA_REGX.match(cem.text):
+                    name = cem.text
+                else:
+                    name = cem.text.lower()
+
+                # Add entity name, start and stop indices to dictionary
+                entity_list.append((name, cem.start, cem.end))
+
+                # Search entity in PubChem if not already done
+                if name not in self.entity_to_cid.keys():
+                    c = pcp.get_compounds(name, 'name')
+                    if len(c) == 0:
+                        self.entity_to_cid[name] = [None, None]
+                        self.entity_counts[name] = 1
+                    else:
+                        c = c[0]
+                        cid = c.cid
+                        iupac_name = c.iupac_name
+                        self.entity_to_cid[name] = [cid, iupac_name]
+                        if cid not in self.cid_to_synonyms.keys():
+                            self.cid_to_synonyms[cid] = [name]
+                        else:
+                            self.cid_to_synonyms[cid].append(name)
+                        self.entity_counts[self.entity_to_cid[name][1]] = 1
+                else:
+                    if self.entity_to_cid[name][0] is None:
+                        self.entity_counts[name] += 1
+                    else:
+                        self.entity_counts[self.entity_to_cid[name][1]] += 1
+
+            # Sort named entities by location in abstract and replace with
+            # synonym
+            entity_list.sort(key=lambda x:x[1])
+            index_change = 0
+            self.entities_per_abstract[i] = []
+            for entity in entity_list:
+                name, start, stop = entity
+                if self.entity_to_cid[name][1] is not None:
+                    replacement_name = self.entity_to_cid[name][1]
+                else:
+                    replacement_name = name
+                if write_bold:
+                    replacement_delta = len(replacement_name) - (stop - start) + 8
+                    abstract = abstract[:start+index_change] + '\033[1m' + replacement_name + '\033[0m' + abstract[stop+index_change:]
+                else:
+                    replacement_delta = len(replacement_name) - (stop - start)
+                    abstract = abstract[:start+index_change] + replacement_name + abstract[stop+index_change:]
+                index_change += replacement_delta
+                self.entities_per_abstract[i].append((replacement_name, start+index_change-replacement_delta, stop+index_change))
+            if verbose:
+                print(abstract)
+                print('\n')
+
+            self.normalized_abstracts.append(abstract)
+
+        if save:
+            os.makedirs('preprocessor_files', exist_ok=True)
+            np.save('preprocessor_files/normalized_abstracts.npy', np.array(self.normalized_abstracts)) # all abstracts
+
+            for cid, synonyms in self.cid_to_synonyms.items():
+                self.cid_to_synonyms[cid] = list(set(synonyms))
+            search_history = {'entity_to_cid': self.entity_to_cid,
+                              'cid_to_synonyms': self.cid_to_synonyms}
+
+            run_history = {'entities_per_abstract': self.entities_per_abstract,
+                           'entity_counts': self.entity_counts}
+
+            with io.open('preprocessor_files/search_history.json', 'w', encoding='utf8') as f: # map of entity names to CID/iupac names for quick recall
+                out_ = json.dumps(search_history,                                              # map of CIDs to all unique synonyms
+                                  indent=4, sort_keys=False,
+                                  separators=(',', ': '), ensure_ascii=False)
+                f.write(str(out_))
+            with io.open('preprocessor_files/run_history.json', 'w', encoding='utf8') as f: # list of entities in each abstract with span
+                out_ = json.dumps(run_history,                                              # dictionary of unique entities and their number of occurrences
+                                  indent=4, sort_keys=False,
+                                  separators=(',', ': '), ensure_ascii=False)
+                f.write(str(out_))
+
 
     def clean_abstract(self, abstract):
         """
@@ -175,4 +346,59 @@ class PreProcessor():
         else:
             clean_abstract = abstract
 
+        if clean_abstract.endswith(' Crown Copywrite'):
+            clean_abstract.replace(' Crown Copywrite', '')
+
         return clean_abstract
+
+    def remove_abbreviations(self, abstract):
+        doc = Document(abstract)
+        abbvs = doc.abbreviation_definitions
+        cems = doc.cems
+        if len(abbvs) > 0:
+            abbv_dict = {}
+            for abbv in abbvs:
+                cem_starts = []
+                cem_ends = []
+                if abbv[-1] is not None:
+                    abbv_dict[abbv[0][0]] = [' '.join(abbv[1])]
+                    for cem in cems:
+                        if cem.text == abbv[0][0]:
+                            cem_starts.append(cem.start)
+                            cem_ends.append(cem.end)
+                    if len(cem_starts) > 0:
+                        low_idx = cem_starts[np.argmin(cem_starts)]
+                    else:
+                        low_idx = 0
+                    abbv_dict[abbv[0][0]].append(low_idx)
+            abbv_dict = {k: v for k, v in sorted(abbv_dict.items(), key=lambda item: item[1][1])}
+            index_change = 0
+            for abbv in abbv_dict.keys():
+                non_abbv = abbv_dict[abbv][0]
+                if abbv_dict[abbv][1] != 0:
+                    replacement_delta = len(non_abbv) - len(abbv)
+                    cem_starts = []
+                    cem_ends = []
+                    for cem in cems:
+                        if cem.text == abbv:
+                            cem_starts.append(cem.start)
+                            cem_ends.append(cem.end)
+                    if len(cem_starts) == 1:
+                        if abstract[cem_starts[0]+index_change-1]+abstract[cem_ends[0]+index_change] == '()':
+                            abstract = abstract[:cem_starts[0]-2+index_change] + abstract[cem_ends[0]+1+index_change:]
+                            index_change += cem_starts[0] - cem_ends[0] - 3
+                        else:
+                            pass
+                    else:
+                        low_idx = np.argmin(cem_starts)
+                        cem_start_low = cem_starts[low_idx]
+                        cem_end_low = cem_ends[low_idx]
+                        if abstract[cem_start_low+index_change-1]+abstract[cem_end_low+index_change] == '()':
+                            abstract = abstract[:cem_start_low-2+index_change] + abstract[cem_end_low+1+index_change:]
+                            index_change += cem_start_low - cem_end_low - 3
+                        else:
+                            pass
+                    abstract = re.sub(r'([\s]){}([.,;\s]|$)'.format(abbv), r' {}\2'.format(non_abbv), abstract)
+                else:
+                    pass
+        return abstract
