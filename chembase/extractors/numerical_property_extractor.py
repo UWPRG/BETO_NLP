7c72ef7dcd8f5fa02ba280577749b393d60417ee
Wesley Tatum <wesleyktatum@gmail.com>
2021-03-11 15:40:34 -0800

    update documentation for property extractor. filter and identify corrosion inhibitors

--- chembase/extractors/numerical_property_extractor.py
+++ chembase/extractors/numerical_property_extractor.py
@@ -66,7 +66,14 @@ class PropertyExtractor():
     
     def extract_from_string(self, article_string):
         """
+        Converts a full text article string to a cde.Document() and extracts chemical records
         
+        Parameters:
+            article_string (str): string containing the article text
+            
+        Returns:
+            records (json): serialized version of cde.Document.records() that lists entities
+                and their detected properties
         """
         doc = cde.Document.from_string(article_string)
         records = doc.records.serialize()
@@ -77,7 +84,15 @@ class PropertyExtractor():
         
     def extract_from_pdf(self, article_pdf):
         """
+        Converts a full text article pdf to a cde.Document() and extracts chemical records
         
+        Parameters:
+            article_pdf (str): string containing the path to the pdf article. Reader determined by self.
+                reader
+            
+        Returns:
+            records (json): serialized version of cde.Document.records() that lists entities
+                and their detected properties
         """
         
         f = open(pdf_path, 'rb')
@@ -91,7 +106,15 @@ class PropertyExtractor():
     
     def extract_from_html(self, html_string):
         """
+        Converts a full text article HTML to a cde.Document() and extracts chemical records
         
+        Parameters:
+            html_string (str): string containing the article HTML. Reader determined by self.
+                reader
+            
+        Returns:
+            records (json): serialized version of cde.Document.records() that lists entities
+                and their detected properties
         """
         html_string = html_string.encode()
         
@@ -103,7 +126,15 @@ class PropertyExtractor():
         
     def extract_from_json(self, article_json):
         """
+        Converts a full text article json to a cde.Document() and extracts chemical records
         
+        Parameters:
+            html_string (json): dict containing the article text sections. Assumes parsed Elsevier
+                fulltext format
+            
+        Returns:
+            records (json): serialized version of cde.Document.records() that lists entities
+                and their detected properties
         """
         if self.publisher == 'Elsevier':
             article_text = self.load_elsevier_json(article_json)
@@ -119,7 +150,14 @@ class PropertyExtractor():
         
     def load_elsevier_json(self, article_json):
         """
+        Loads a parsed Elsevier article json. Stitches together sections into single string
+        for easier parsing
         
+        Parameters:
+            article_json (json): dict containing article full text and metadata
+            
+        Returns:
+            article_text (str): the combined article sections as a single string
         """
         sfp = ecg.SciFullTextProcessor()
         
@@ -148,7 +186,15 @@ class PropertyExtractor():
     
     def extract(self, article):
         """
+        According to the article format (as declared in  self.__ini__()), applies appropriate
+        extraction functions.
         
+        Parameters:
+            article (?): article of pre-specified format to be parsed
+            
+        Returns:
+            records (json): serialized version of cde.Document.records() that lists entities
+                and their detected properties
         """
         
         if self.format == 'HTML':
@@ -169,7 +215,19 @@ class PropertyExtractor():
     
     def filter_properties(self, raw_records, recs_with_props_only = 'False'):
         """
+        Remove uneccesary entity information from the raw records returned by self.extract(). If
+        recs_with_props_only is True, then only chemical records that have more than a name is
+        added to the dictionary of results that is returned.
         
+        Parameters:
+            raw_records (json): serialized version of cde.Document.records() that lists entities
+                and their detected properties
+                
+            recs_with_props_only (bool): determines whether all chemical records are returned or
+                just those that also have properties.
+                
+        Returns:
+            new_recs (json): dictionary containing the refined and maybe filtered records.
         """
         new_recs = []
         
@@ -198,7 +256,20 @@ class PropertyExtractor():
     
     def reformat_list_of_records(self, records_list, doi):
         """
+        This function converts the records dictionary to the format that will be used to store
+        the extracted compound/property information in the database collection MolecularEntities
         
+        Parameters:
+            records_list (list of json): list of chemical records, perhaps
+                for a single article. For each record, creates newly formatted dict and appends
+                to the list of new records, `reformatted`
+                
+            doi (list of str): list of DOI that each record is associated with. This is required
+                so that each property in the database has an associated reference
+                
+        Returns:
+            reformatted (list of json): the list of newly reformatted properties json. This can
+                be iterated through to directly update the MolecularEntities collection.
         """
         reformatted = []
 
